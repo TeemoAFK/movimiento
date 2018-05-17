8e4e455e9c08a70389cd818c386f72f3c28b0592
MATEO\OscarMateo <oscar.penaherrera@epn.edu.ec>
2018-05-17 10:54:47 -0500

    Codigo listo

--- Movimiento/venv/Include/main.py
+++ Movimiento/venv/Include/main.py
@@ -5,11 +5,13 @@ canvas.pack()
 fondo=PhotoImage(file="fondo.gif")
 goku=PhotoImage(file="goku.png")
 freezer=PhotoImage(file="freezer.png")
+poder1=PhotoImage(file="1.png")
 canvas.create_image(0,0,anchor=NW,image=fondo)
 canvas.create_image(1000,400,anchor=NW,image=goku)
 canvas.create_image(80,350,anchor=NW,image=freezer)
+canvas.create_image(70,350,anchor=NW,image=poder1)
 
-#def movetriangle(event):
-#    canvas.move(1,5,0)
-#canvas.bind_all('<KeyPress-Return>',movetriangle)
+def movetriangle(event):
+    canvas.move(4,5,0)
+canvas.bind_all('<KeyPress-Return>',movetriangle)
 tk.mainloop()
